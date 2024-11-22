2cef3df36cf98f61bf90807a68295aa78998c1bc
Valeria <valeria.casatti@gmail.com>
2024-11-22 11:55:46 -0300

    ejercicios II

---
+++ clase01/src/main/java/com/ejemplo/ClaseA.java
@@ -0,0 +1,9 @@
+package com.ejemplo;
+
+public class ClaseA {
+
+	public void metodoClaseA() {
+		System.out.println("Método de Clase A ejecutado.");
+	}
+
+}
---
+++ clase01/src/main/java/com/ejemplo/ClaseB.java
@@ -0,0 +1,9 @@
+package com.ejemplo;
+
+public class ClaseB {
+
+	public void metodoClaseB() {
+		System.out.println("Método de Clase B ejecutado.");
+	}
+
+}
